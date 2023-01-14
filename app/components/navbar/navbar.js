f2935d855797be8ddb0c8ac172e4c988a760eb63
matthias-blumenschein <matthiasblumenschein@gmail.com>
2023-01-14 16:03:34 +0100

    set fontfamily, design logo

--- app/components/navbar/navbar.js
+++ app/components/navbar/navbar.js
@@ -7,7 +7,11 @@ function Navbar() {
 	return (
 		<nav className={styles.navbar}>
 			<div className={styles.container}>
-				<p>Movie Database</p>
+				<div className={styles.headingContainer}>
+					<p className={`${styles.heading} ${styles.heading1}`}>FILM</p>
+					<p className={`${styles.heading} ${styles.heading2}`}>DATA</p>
+					<p className={`${styles.heading} ${styles.heading3}`}>BASE</p>
+				</div>
 				<SearchBar />
 				<button>Filter</button>
 			</div>
