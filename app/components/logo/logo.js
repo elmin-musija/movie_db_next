4e74a9e4af48d58bb0c74bb21ad7dc34e25759d7
matthias-blumenschein <matthiasblumenschein@gmail.com>
2023-01-17 17:27:34 +0100

    make logo a link to home

--- app/components/logo/logo.js
+++ app/components/logo/logo.js
@@ -1,12 +1,15 @@
 import React from "react";
+import Link from "next/link";
 import styles from "./logo.module.css";
 
 function Logo() {
 	return (
 		<div className={styles.headingContainer}>
-			<p className={`${styles.heading} ${styles.heading1}`}>FILM</p>
-			<p className={`${styles.heading} ${styles.heading2}`}>DATA</p>
-			<p className={`${styles.heading} ${styles.heading3}`}>BASE</p>
+			<Link href="/">
+				<p className={`${styles.heading} ${styles.heading1}`}>FILM</p>
+				<p className={`${styles.heading} ${styles.heading2}`}>DATA</p>
+				<p className={`${styles.heading} ${styles.heading3}`}>BASE</p>
+			</Link>
 		</div>
 	);
 }
